6741cffd183d80de22241125bd275dc8b091c38a
luuphuc144244 <157662701+luuphuc144244@users.noreply.github.com>
2024-03-14 07:45:19 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,20 @@
-public class Item {
+public abstract class Item {
+    public String name;
+    private String description;
+    protected String ID;
+    int price;
+    void showinfo() {
+    }
+    public Item(String name, String description, String ID, int price) {
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.price = price;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
