d67464f9add6425a11ae0f8ebd36899bb6c903b7
luuphuc144244 <157662701+luuphuc144244@users.noreply.github.com>
2024-03-14 07:47:04 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        MP3 mp3;
-        mp3.showInfo();
-
-        Book book;
-        book.showInfo();
-
+        MP3 mp3 = new MP3("File MP3", "Nhac nuoc ngoai", "0001", 100, 120);
+        Book book = new Book("File Book", "Truyen ngan ban chay", "0002", 200, "Nguyen Anh", 180,"Truyen ngan");
+        mp3.showinfo();
+        book.showinfo();
     }
 }
